7da9ad0e052e3048ee8194f2a4bffb21c38e8894
AMOREM <amorem@amorem.com>
2019-05-05 08:37:52 +0200

    Initial commit

---
+++ src/components/home/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "../globals/Hero";
+const Header = () => {
+  return (
+    <Hero>
+      <h1>Hello</h1>
+    </Hero>
+  );
+};
+
+export default Header;
